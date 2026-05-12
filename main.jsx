c458eccc4d25d5cf1414e1b3c1b70ebceb7c6351
firdavstoychiboyev-cmyk <firdavstoychiboyev@gmail.com>
2026-05-12 23:23:39 +0500

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './uniprep_ver_.jsx'
+import App from './uniprep_ver_.jsx' // Убедись, что имя файла совпадает!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
